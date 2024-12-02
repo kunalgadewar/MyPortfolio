85f6682f7aab13204dffaf65da5663ae2c23b615
kunalgadewar <kunalgadewar03@gmail.com>
2024-12-02 15:07:40 +0530

    ADD New Lines

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {BrowserRouter as Router } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router future={{ v7_relativeSplatPath: true }}>
-    <App />
+    <App/>
   </Router>
 );
 
